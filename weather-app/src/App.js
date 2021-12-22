04add7c7c3b895f3d1ee6fb2c333b0cc75f827a3
amandadomer <amdomer@icloud.com>
2021-12-21 20:07:56 -0600

    app set up. added header.

---
+++ weather-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div> 
+      <span className="header">
+        what's it like outside?
+      </span>
+    </div>
+  )
+}
+
+export default App;
