ef03363380def3c11635e48015dff4da0485cece
amandadomer <amdomer@icloud.com>
2021-12-21 21:44:17 -0600

    search bar added

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,5 +1,13 @@
-import React from 'react';
+import {React, useState} from 'react';
 import './App.css';
+import SearchBar from './searchBar';
+import axios from 'axios';
+
+const api = {
+  key: "b55c4ba2f2msh85e958ceff4c00ap110430jsnec27170bb0c7",
+  base: "community-open-weather-map.p.rapidapi.com"
+}
+
 
 function App() {
   return (
@@ -7,6 +15,8 @@ function App() {
       <span className="header">
         what's it like outside?
       </span>
+      <SearchBar/>
+
     </div>
   )
 }
